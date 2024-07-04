08ca7fefd916bf42fa6877b3878afdddb3941afe
Bisesh Adhikari <adhikaribisesh1590@gmail.com>
2024-07-04 16:45:18 +0545

    models added

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from app.models import Todolist
 # Create your views here.
 
 
@@ -11,11 +11,28 @@ def calculator(request):
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         
-       
+          
+    name_by_get_method = request.GET.get('name') 
+    last_name_by_get_method = request.GET.get('last_name')
     context = {
         'first_name':first_name,
-        'last_name':last_name
+        'last_name':last_name,
+        'name_by_get_method':name_by_get_method,
+        'last_name_by_get_method':last_name_by_get_method
     }
     
+    
     return render(request,'calculator.html',context)
+
+
+
+def index(request):
+    todolists = Todolist.objects.all()
+    names = ['bisesh','sushant','saroj']
+  
+    context = {
+        'todolists':todolists
+    }
+    return render(request,'index.html',context)
+
     
